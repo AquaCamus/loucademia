4a078681d83d5aa1c3bcf1b3a5fe6e77e0643846
Gabriel Viana <50640703+AquaCamus@users.noreply.github.com>
2020-02-21 17:35:16 -0300

    Projeto Criado

---
+++ src/br/com/jg/loucademia/Config.java
@@ -0,0 +1,12 @@
+package br.com.jg.loucademia;
+
+import javax.faces.annotation.FacesConfig;
+import javax.faces.bean.ApplicationScoped;
+
+import com.sun.tools.javac.jvm.ClassFile.Version;
+
+@FacesConfig(version = javax.faces.annotation.FacesConfig.Version.JSF_2_3)
+@javax.enterprise.context.ApplicationScoped
+public class Config {
+	
+}
